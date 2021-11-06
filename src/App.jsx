28e419d7945a8f5ca145b758046e3ddd59540cad
Hodeem <miller.hodeem@gmail.com>
2021-11-05 20:58:01 -0500

    Added all upper page elements and started styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 
+import CardIcon from './icons/Card.svg'
+import DeckIcon from './icons/Deck.svg'
+
 import CardsBlock from './components/CardsBlock'
 
 import DataService from './lib/DataService'
@@ -137,53 +140,53 @@ function App() {
 
     const AscendingCompareFn = (firstObject, secondObject) => {
 
-        if(sortBy == 'homeworld'){
+        if (sortBy == 'homeworld') {
             var firstEl = firstObject.homeworld;
             var secondEl = secondObject.homeworld;
         }
-    
-        if(sortBy == 'vehicles'){
+
+        if (sortBy == 'vehicles') {
             var firstEl = firstObject.vehicles.length;
             var secondEl = secondObject.vehicles.length;
         }
-    
-        if(sortBy == 'starships'){
+
+        if (sortBy == 'starships') {
             var firstEl = firstObject.starships.length;
             var secondEl = secondObject.starships.length;
         }
-    
+
         if (firstEl < secondEl) return -1;
         if (secondEl < firstEl) return 1;
         return 0;
     };
-    
+
     const DescendingCompareFn = (firstObject, secondObject) => {
-    
-        if(sortBy == 'homeworld'){
+
+        if (sortBy == 'homeworld') {
             var firstEl = firstObject.homeworld;
             var secondEl = secondObject.homeworld;
         }
-    
-        if(sortBy == 'vehicles'){
+
+        if (sortBy == 'vehicles') {
             var firstEl = firstObject.vehicles.length;
             var secondEl = secondObject.vehicles.length;
         }
-    
-        if(sortBy == 'starships'){
+
+        if (sortBy == 'starships') {
             var firstEl = firstObject.starships.length;
             var secondEl = secondObject.starships.length;
         }
-    
+
         if (firstEl > secondEl) return -1;
         if (secondEl < firstEl) return 1;
         return 0;
     };
-    
+
     const sortAscending = (unsortedArray) => {
         let sortedArray = [...unsortedArray].sort(AscendingCompareFn);
         return sortedArray;
     };
-    
+
     const sortDescending = (unsortedArray) => {
         let sortedArray = [...unsortedArray].sort(DescendingCompareFn);
         return sortedArray;
@@ -204,15 +207,40 @@ function App() {
 
     return (
         <div className="App">
-            <button onClick={() => extractAllRaw()}> All Cards </button>
-            <select id="sortBy" onChange={handleSelectChange}>
-                <option value="homeworld">Homeworld</option>
-                <option value="starships">Starships</option>
-                <option value="vehicles">Vehicles</option>
-            </select>
+            <div className="top-block">
+                <div className="top-btn-bar">
+                    <span className='top-btn-bar-left'>
+                        <button className='btn-top' onClick={() => extractAllRaw()}>
+                            <img src={CardIcon} />
+                            <span> All Cards </span>
+                        </button>
+                        <button className='btn-top' disabled>
+                            <img src={DeckIcon} />
+                            <span> Decks </span>
+                        </button>
+                    </span>
 
-            <button onClick={() => setSortedAsc()}> Asc </button>
-            <button onClick={() => setSortedDesc()}> Desc </button>
+                    <button className='btn-top' disabled> Bavin Edwards </button>
+                </div>
+
+                <div id="route-bar">
+                    <p>Enter dynamic route here</p>
+                </div>
+                <div id="search-bar">
+                    <input type='text' placeholder='Search' id='search'/>
+                </div>
+                <div id="sort-bar">
+                    <select id="sortBy" onChange={handleSelectChange}>
+                        <option value="homeworld">Homeworld</option>
+                        <option value="starships">Starships</option>
+                        <option value="vehicles">Vehicles</option>
+                    </select>
+
+                    <button onClick={() => setSortedAsc()}> Asc </button>
+                    <button onClick={() => setSortedDesc()}> Desc </button>
+                </div>
+            </div>
+
 
             <CardsBlock data={characterDetails} />
         </div>
